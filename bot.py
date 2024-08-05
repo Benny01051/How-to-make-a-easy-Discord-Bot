d477f1e9c9d3ee9814f57239f1f98d2ea373d435
crimeiscool <139124034+crimeiscool@users.noreply.github.com>
2024-08-05 15:47:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,11 +12,11 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync()  # Sync the command tree with Discord
+    await bot.tree.sync()  
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-@bot.tree.command(name='membercount', description='Zeigt dir member von dem Server')
+@bot.tree.command(name='membercount', description='show how many members')
 async def member_count(interaction: discord.Interaction):
     guild = interaction.guild
     member_count = guild.member_count
@@ -27,9 +27,9 @@ async def member_count(interaction: discord.Interaction):
     await interaction.response.send_message(embed=embed)
 
 
-@bot.tree.command(name='nachrichten_löschen')
+@bot.tree.command(name='delete_messages')
 async def clear_messages(ctx, channel: discord.TextChannel):
-    embed = discord.Embed(title="Nachrichten", description="Ich lösche die Nachrichten jetzt", color=discord.Color.red())
+    embed = discord.Embed(title="Alert", description="im deleting messages now", color=discord.Color.red())
     embed_message = await ctx.channel.send(embed=embed)
 
     async for message in channel.history(limit=None):
@@ -44,3 +44,6 @@ async def clear_messages(ctx, channel: discord.TextChannel):
 async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name="Member")
     await member.add_roles(role)
+
+
+bot.run("YOUR BOT TOKEN")
